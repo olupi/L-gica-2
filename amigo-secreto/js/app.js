8d02a5cf2d0ad213ad982590041c79a7ad06a140
olupi <victorfaxzinho@gmail.com>
2025-06-04 21:08:39 -0300

    Ingressos Concluido

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -1,14 +1,46 @@
-let lista de nomes;
+let arrayDeNomes = [];
 
 function adicionar() {
-
+    let listaAmigos = document.getElementById('lista-amigos');
+    let nomeDoAmigo = document.getElementById('nome-amigo').value;
+    arrayDeNomes.push(nomeDoAmigo);
+    
+    if (listaAmigos.textContent == '') {
+        listaAmigos.textContent = nomeDoAmigo;
+    } else {
+        listaAmigos.textContent = listaAmigos.textContent + ', ' + nomeDoAmigo;
+    }
+    document.getElementById('nome-amigo').value = ' ';
 }
 
 function sortear() {
+    embaralha(arrayDeNomes)
+    let listaSorteio = document.getElementById('lista-sorteio');
+
+    for (let i = 0; i < arrayDeNomes.length; i++) {
+        const atual = arrayDeNomes[i];
+        const proximo = arrayDeNomes[(i + 1) % arrayDeNomes.length]; // o truque está aqui
+        listaSorteio.innerHTML += (`${atual} → ${proximo}<br>`);
+    }
 
 }
 
-function reiniciar() {
+function embaralha(arrayDeNomes) {
 
+    for (let indice = arrayDeNomes.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [arrayDeNomes[indice - 1], arrayDeNomes[indiceAleatorio]] = 
+            [arrayDeNomes[indiceAleatorio], arrayDeNomes[indice - 1]];
+    }
+}
+
+function reiniciar() {
+    amigos = []
+    document.getElementById('nome-amigo').value = ' ';
+    document.getElementById('lista-amigos').innerHTML = ' ';
+    document.getElementById('lista-sorteio').innerHTML = ' ';
 }
 
